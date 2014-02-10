fae3addaafdd83bf93ae64cb0f12f1186ae5c86b
Jeff Miller <loadeddesigns@gmail.com>
2014-02-10 01:58:50 -0800

    Start integrating Amazon

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application.
 AudioTrax::Application.initialize!
+
+# Load Amazon S3 Config
+path = File.join(Rails.root, 'config/amazon.yml')
+AMAZON = YAML.load(ERB.new(File.read(path)).result)[Rails.env].symbolize_keys
