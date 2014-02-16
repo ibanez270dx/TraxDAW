d799c6cb551912da98a7482bb25f6a4cb1816913
Jeff Miller <loadeddesigns@gmail.com>
2014-02-15 19:30:33 -0800

    Add track functionality, add variables scss

--- app/controllers/editor_controller.rb
+++ app/controllers/editor_controller.rb
@@ -3,4 +3,12 @@ class EditorController < ApplicationController
   def index
   end
 
+  ##################################
+  # JSON actions
+  ##################################
+  
+  def add_track
+    render json: { html: render_to_string(partial: 'track', formats: [:html]) }
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ AudioTrax::Application.routes.draw do
 
   root to: 'editor#index'
 
+  controller :editor do
+    get 'editor/add-track', action: 'add_track', as: 'add_track'
+  end
 end
