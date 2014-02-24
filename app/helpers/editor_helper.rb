88aef6ef2934f94e063d793555080efb435a1e3f
Jeff Miller <loadeddesigns@gmail.com>
2014-02-23 17:45:40 -0800

    Start refactoring editor ui, add button helper

--- app/helpers/editor_helper.rb
+++ app/helpers/editor_helper.rb
@@ -1,2 +1,10 @@
 module EditorHelper
+
+  def switch_icon(name)
+    content_tag(:div, class: 'switch-icon') do 
+      check_box_tag("#{name}-on") +
+      label_tag("#{name}-on") { content_tag(:i, nil, class: "icon-#{name}") }
+    end
+  end
+
 end
