881ec8cbd457783931719de85a087555e44af709
Jeff Miller <loadeddesigns@gmail.com>
2014-02-23 23:22:01 -0800

    Get switch-icon UI working

--- app/helpers/editor_helper.rb
+++ app/helpers/editor_helper.rb
@@ -1,9 +1,10 @@
 module EditorHelper
 
-  def switch_icon(name)
-    content_tag(:div, class: 'switch-icon') do 
-      check_box_tag("#{name}-on") +
-      label_tag("#{name}-on") { content_tag(:i, nil, class: "icon-#{name}") }
+  def switch_icon(name, options={})
+    options = { name: name }.merge(options)
+    content_tag(:div, class: "switch-icon #{options[:name]}") do 
+      check_box_tag("#{options[:name]}-on") +
+      label_tag("#{options[:name]}-on") { content_tag(:i, nil, class: "icon-#{name} inset") }
     end
   end
 
